87e23b9611c2fc7104877322e501c9eba618bb21
Nguyen Phuong <n21dccn166@student.ptithcm.edu.vn>
2024-06-22 16:46:38 +0700

    Merge đủ 3 dataset

---
+++ QuanLyVatTuPhanTan/FormPhieuXuat.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyVatTuPhanTan
+{
+    public partial class FormPhieuXuat : DevExpress.XtraEditors.XtraForm
+    {
+        public FormPhieuXuat()
+        {
+            InitializeComponent();
+        }
+
+        private void FormPhieuXuat_Load(object sender, EventArgs e)
+        {
+            this.phieuXuatTableAdapter.Fill(this.dataSet_Phuong.PhieuXuat);
+            this.cTPXTableAdapter.Fill(this.dataSet_Phuong.CTPX);
+        }
+    }
+}
