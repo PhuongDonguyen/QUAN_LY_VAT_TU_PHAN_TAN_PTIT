419f8c6abfeb93964b69310c278bf776aa644a2a
Nguyen Phuong <n21dccn166@student.ptithcm.edu.vn>
2024-06-25 21:27:02 +0700

    tạo giao diện form report nhập/xuất hàng

---
+++ QuanLyVatTuPhanTan/ReportForm/FormChiTietSoLuongTriGiaHangHoaNhapXuat.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace QuanLyVatTuPhanTan.ReportForm
+{
+    partial class FormChiTietSoLuongTriGiaHangHoaNhapXuat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dteToiNgay = new DevExpress.XtraEditors.DateEdit();
+            this.dteTuNgay = new DevExpress.XtraEditors.DateEdit();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbLoaiPhieu = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dteToiNgay.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dteToiNgay.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dteTuNgay.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dteTuNgay.Properties.CalendarTimeProperties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHI TIẾT SỐ LƯỢNG HÀNG NHẬP XUẤT";
+            // 
+            // dteToiNgay
+            // 
+            this.dteToiNgay.EditValue = null;
+            this.dteToiNgay.Location = new System.Drawing.Point(434, 210);
+            this.dteToiNgay.Margin = new System.Windows.Forms.Padding(5);
+            this.dteToiNgay.Name = "dteToiNgay";
+            this.dteToiNgay.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dteToiNgay.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dteToiNgay.Size = new System.Drawing.Size(189, 20);
+            this.dteToiNgay.TabIndex = 12;
+            // 
+            // dteTuNgay
+            // 
+            this.dteTuNgay.EditValue = null;
+            this.dteTuNgay.Location = new System.Drawing.Point(171, 210);
+            this.dteTuNgay.Margin = new System.Windows.Forms.Padding(5);
+            this.dteTuNgay.Name = "dteTuNgay";
+            this.dteTuNgay.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dteTuNgay.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dteTuNgay.Size = new System.Drawing.Size(156, 20);
+            this.dteTuNgay.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(356, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tới Ngày";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(96, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Từ Ngày";
+            // 
+            // cmbLoaiPhieu
+            // 
+            this.cmbLoaiPhieu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLoaiPhieu.FormattingEnabled = true;
+            this.cmbLoaiPhieu.Items.AddRange(new object[] {
+            "NHAP",
+            "XUAT"});
+            this.cmbLoaiPhieu.Location = new System.Drawing.Point(202, 122);
+            this.cmbLoaiPhieu.Name = "cmbLoaiPhieu";
+            this.cmbLoaiPhieu.Size = new System.Drawing.Size(125, 21);
+            this.cmbLoaiPhieu.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Loại Phiếu";
+            // 
+            // FormChiTietSoLuongTriGiaHangHoaNhapXuat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 361);
+            this.Controls.Add(this.dteToiNgay);
+            this.Controls.Add(this.dteTuNgay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbLoaiPhieu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormChiTietSoLuongTriGiaHangHoaNhapXuat";
+            this.Text = "Chi tiết số lượng nhập/xuất hàng";
+            ((System.ComponentModel.ISupportInitialize)(this.dteToiNgay.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dteToiNgay.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dteTuNgay.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dteTuNgay.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.DateEdit dteToiNgay;
+        private DevExpress.XtraEditors.DateEdit dteTuNgay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbLoaiPhieu;
+        private System.Windows.Forms.Label label2;
+    }
+}
